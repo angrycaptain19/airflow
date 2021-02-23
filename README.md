42baba0e95b8a9b2e6dce7fde3520f3f6b47556c
patrik.racskai@gmail.com <patrik.racskai@continental-corporation.com>
2021-02-23 07:16:49 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,41 @@
+# Infrastructure Engineer Case Study
+
+## Provision Kubernetes cluster
+
+`kind create cluster --config kind-config.yml`
+
+## Deploy Airflow on the cluster along with database
+
+`kubectl apply -f database-secrets.yml`
+`helm install airflow bitnami/airflow --values values.yaml`
+
+`kubectl get nodes --namespace default -o jsonpath="{.items[0].status.addresses[0].address}"`
+
+`kubectl get --namespace default -o jsonpath="{.spec.ports[0].nodePort}" services airflow`
+
+Wait till apps are ready
+`kubectl wait --for=condition=ready pod -l app.kubernetes.io/instance=airflow --timeout=3600s`
+
+Once all pods are up and running 
+Open `http://172.18.0.3:31759/` in a browser
+
+Login with `user` username and for retrieving the password execute the following command:
+`kubectl get secret --namespace "default" airflow -o jsonpath="{.data.airflow-password}" | base64 --decode`
+
+
+## Use Airflow to load data from the `data/` directory to the database
+
+Trigger `initial_load_dag` DAG.
+
+
+Get the database password by running:
+
+`kubectl get secret --namespace "default" database-secrets -o jsonpath="{.data.POSTGRESQLPASSWORD}" | base64 --decode``
+
+When pipline is completed check the table content in PostgreSQL:
+
+kubectl exec -it airflow-postgresql-0 bash
+I have no name!@airflow-postgresql-0:/$ psql -U bn_airflow -d bitnami_airflow -c 'select * from stores'
+I have no name!@airflow-postgresql-0:/$ psql -U bn_airflow -d bitnami_airflow -c 'select * from events'
+`
+
---
+++ dags/dag.py
@@ -0,0 +1,135 @@
+import os
+import requests
+import json
+import psycopg2 as pg
+from datetime import date
+
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from datetime import datetime, timedelta
+
+def initialLoadStores():
+    dbconnect = None
+    try:
+        dbconnect = pg.connect(
+            database=os.environ['POSTGRESQLDATABASE'],
+            user=os.environ['POSTGRESQLUSERNAME'],
+            password=os.environ['POSTGRESQLPASSWORD'],
+            host=os.environ['POSTGRESQLHOST'],
+            port=os.environ['POSTGRESQLPORT']
+        )
+
+        cursor = dbconnect.cursor()
+
+        cursor.execute("""
+            CREATE TABLE IF NOT EXISTS stores (
+                id SERIAL,
+                storenr INTEGER UNIQUE,
+                close_date text,
+                address varchar(255),
+                warehousenr INTEGER,
+                PRIMARY KEY (id)
+            );
+
+            TRUNCATE TABLE stores CASCADE;
+        """
+        )
+
+        dbconnect.commit()
+
+        with open("/opt/bitnami/airflow/dags/git-airflow-dat/data/data/initial_load/stores.csv", 'r', encoding='unicode_escape') as reader:
+            next(reader, None)
+            for row in reader:
+                cursor.execute("""
+                    INSERT INTO stores(storenr, close_date, address, warehousenr)
+                    VALUES ('{}', '{}', '{}', '{}')
+                """.format(
+                row.split(";")[0],
+                row.split(";")[1],
+                row.split(";")[2],
+                row.split(";")[3])
+                )
+        dbconnect.commit()
+
+        cursor.close()
+    except (Exception, pg.DatabaseError) as error:
+        print(error)
+
+    finally:
+        if dbconnect is not None:
+            dbconnect.close()
+            print('Connection to the database is closed')
+
+def initialLoadEvents():
+    dbconnect = None
+    try:
+        dbconnect = pg.connect(
+            database=os.environ['POSTGRESQLDATABASE'],
+            user=os.environ['POSTGRESQLUSERNAME'],
+            password=os.environ['POSTGRESQLPASSWORD'],
+            host=os.environ['POSTGRESQLHOST'],
+            port=os.environ['POSTGRESQLPORT']
+        )
+
+        cursor = dbconnect.cursor()
+
+        cursor.execute("""
+            CREATE TABLE IF NOT EXISTS events (
+                id SERIAL,
+                storenr INTEGER,
+                date text,
+                customers INTEGER,
+                PRIMARY KEY (id),
+                FOREIGN KEY (storenr) REFERENCES stores (storenr)
+            );
+
+            TRUNCATE TABLE events CASCADE;
+        """
+        )
+
+        dbconnect.commit()
+
+        with open("/opt/bitnami/airflow/dags/git-airflow-dat/data/data/initial_load/customer_number.csv", 'r', encoding='unicode_escape') as reader:
+            next(reader, None)
+            for row in reader:
+                cursor.execute("""
+                    INSERT INTO events(storenr, date, customers)
+                    VALUES ('{}', '{}', '{}')
+                """.format(
+                row.split(",")[0],
+                row.split(",")[1],
+                row.split(",")[2])
+                )
+        dbconnect.commit()
+
+        cursor.close()
+    except (Exception, pg.DatabaseError) as error:
+        print(error)
+
+    finally:
+        if dbconnect is not None:
+            dbconnect.close()
+            print('Connection to the database is closed')
+
+
+default_args = {
+    "owner": "airflow",
+    "start_date": datetime.today() - timedelta(days=1)
+}
+dag = DAG(
+    "initial_load_dag",
+    default_args=default_args,
+    schedule_interval=None,
+    )
+initialLoadStores = PythonOperator(
+    task_id="initial_load_stores",
+    python_callable=initialLoadStores,
+    dag=dag,
+    )
+initialLoadEvents = PythonOperator(
+    task_id="initial_load_events",
+    python_callable=initialLoadEvents,
+    dag=dag,
+    )
+
+initialLoadStores >> initialLoadEvents
